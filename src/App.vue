19dbcac51f952fe5d97b4768ee11a2c42e2452cc
Kaloid <poker6-96@mail.ru>
2022-03-14 15:50:23 +0200

    change

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,92 @@
 <template>
-	<div>
-		Работает
+	<div class="app">
+		<div>
+			<form @submit.prevent>
+				<input 
+					v-bind:value="title" 
+					@input="title = $event.target.value"
+					class="input" 
+					type="text" 
+					placeholder="Название"
+				>
+				<input 
+					v-bind:value="body"
+					@input="body = $event.target.value"
+					class="input" 
+					type="text" 
+					placeholder="Описание"
+				>
+				<button class="btn" @click="createPost">Создать</button>
+			</form>
+		</div>
+		<div class="post" v-for="post in posts" :key="post">
+			<div><strong>Название:</strong>{{post.title}}</div>
+			<div><strong>описание:</strong>{{post.body}}</div>
+		</div>
 	</div>
 </template>
 <script>
 export default {
-	
+	data(){
+		return {
+			posts: [
+				{id: 1, title: 'JavaScript', body:'Описание поста'},
+				{id: 2, title: 'JavaScript 2', body:'Описание поста 2'},
+				{id: 3, title: 'JavaScript 3', body:'Описание поста 3'},
+				{id: 3, title: 'JavaScript 4', body:'Описание поста 4'},
+			],
+			title: '',
+			body: ''
+		}
+	},
+	methods: {
+		createPost() {
+			const newPost = {
+				id: Date.now(),
+				title: this.title,
+				body: this.body,
+			}
+			this.posts.push(newPost);
+		}
+
+	}
 }
 </script>
 <style>
+* {
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+}
+.app {
+	margin:20px
+}
 
+.post {
+	padding:15px;
+	border:2px solid teal;
+	margin:15px 0 0 0;
+}
+
+form {
+	display:flex;
+	flex-direction:column;
+}
+
+.input {
+	width:100%;
+	border:2px solid teal;
+	padding:10px 15px;
+	margin-top:15px;
+}
+
+.btn {
+	margin-top: 15px;
+	align-self:flex-end;
+	padding:10px 15px;
+	background:none;
+	color:teal;
+	border:1px solid teal;
+	cursor:pointer
+}
 </style>
